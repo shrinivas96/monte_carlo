bfd08aba689f4ea10cf8b2680d6c3e1f28f3c848
shrinivas96 <shrinivasiyengar@live.com>
2020-10-25 15:33:21 +0100

    added comments

--- naive_random_walk.py
+++ naive_random_walk.py
@@ -1,3 +1,10 @@
+"""
+From Socrates YouTube tutorial on monte carlo and random walk:
+The question is: What is the maximum number of walks you can take
+so that the final distance from the start is 4 or less. If its more 
+than 4 then you neeed a transport back home. 
+"""
+
 import random
 
 def random_walk(steps):
@@ -32,16 +39,16 @@ def naive_RW():
 
 def main():
     number_of_walks = 30000
-    for walk_length in range(1, 31):
-        no_transport = 0
-        for _ in range(number_of_walks):
-            (x, y) = comp_random_walk(walk_length)
-            distance = abs(x) + abs(y)
-            if distance <= 4:
-                no_transport += 1
-        no_trans_percent = float(no_transport) / number_of_walks
+    for walk_length in range(1, 31):                                # Increase number of steps every time
+        no_transport = 0                                            # Count the number of times we do not need a transport
+        for _ in range(number_of_walks):                            # MC number_of_walks times simulation
+            (x, y) = comp_random_walk(walk_length)                  # randomly walk walk_length steps
+            distance = abs(x) + abs(y)                              # calculate distance from start point
+            if distance <= 4:                               
+                no_transport += 1                                   # increase counter if no transport needed
+        no_trans_percent = float(no_transport) / number_of_walks    # percentage of time we did not need transport
         print("Walk size =", walk_length, 
-              " and % of no transports =", no_trans_percent * 100)
+              " and % of no transports =", no_trans_percent * 100)  # print it for each walk
 
 if __name__ == "__main__":
     main()
