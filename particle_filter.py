239ab1bbc35839fc2cf3706fc21e6b2df4bc2a71
shrinivas96 <shrinivasiyengar@live.com>
2021-01-04 22:19:39 +0100

    added resampling part. code doesn't work yet because weights are always 0

--- particle_filter.py
+++ particle_filter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from filterpy import monte_carlo
 import samples_and_measurements as sm
 
 # Global contants for needed throughout
@@ -8,26 +9,40 @@ rad2deg = 180 / np.pi
 T = 0.01                                                # sampling time. new measuements at every T seconds
 I = np.identity(3)
 zero_mean = np.array([0.0, 0.0, 0.0])                   # to construct any N(0, Cov) 
+np.random.seed(0)
 
 
-def state_likelihood(z_t, mean, cov):
+def state_likelihood(measurement_t, state_t):
     """
-    Returns the multivariate gaussian likelihood of z_t given x_t.
+    Returns the multivariate gaussian likelihood of measurement_t given state_t.
+    That is, p(z_t | x_t). x_t is the hypothetical state particle.
+
     Parameters:
-    z_t: 2x1 vector of the observed measurement
-    mean: 2x1 vector of expected measurement, mean = C x_t
-    cov: 2x2 measurement noise matrix (pos-semi-def)
+    measurement_t: 2x1 vector of the observed measurement
+    state_t: 2x1 vector of hypothetical state. 
     
-    L(z_t) = (2 pi)^(-k/2) * |cov|^(-1/2) * e ^ {-0.5 * (z - mu)^T . cov^(-1) . (z - mu)}
+    Returns the likelhood of z_t given x_t:
+    L(z_t | x_t) = (2 pi)^(-k/2) * |cov|^(-1/2) * e ^ {-0.5 * (z - mu)^T . cov^(-1) . (z - mu)}
     """
-    k = z_t.size                                    # dimension of the vector 
-    e1 = (2 * np.pi) ** (-k/2)                      # first expression of likelihood
+    # constants for the measurement model
+    rw = 10 ** (-6)                                 # uncetrainty in gyro measurements
+    r_theta = 5 * (10 ** (-5))                      # uncertainty in accel. measurements
+    Rd = np.array([[r_theta, 0],                    # measurement covariance
+                   [0, rw]])
+    C = np.array([[0, 1, 0], [1, 0, 1]])                    # measurement matrix
 
-    cov_sqrt_det = np.linalg.det(cov) ** (-1/2)     # square root of the determinant of covariance matrix
-    cov_inverse = np.linalg.inv(cov)                # inverse of the covariance matrix
 
-    z_mean_diff = (z_t - mean)                                                  # difference between observed and expected measurement
-    z_mean_diff_T = (z_t - mean).T                                              # transpose of above
+    # obs_noise = np.random.multivariate_normal(expected_zt, Rd)           # adding measurement noise ~ N(Cxt, Rd)
+
+    expected_zt = np.dot(C, state_t)                                       # expected z_t for each hyp. particle x_t
+    k = measurement_t.size                                    # dimension of the vector 
+    e1 = (2 * np.pi) ** (-1/2)                      # first expression of likelihood
+
+    cov_sqrt_det = np.linalg.det(Rd) ** (-1/2)     # square root of the determinant of covariance matrix
+    cov_inverse = np.linalg.inv(Rd)                # inverse of the covariance matrix
+
+    z_mean_diff = (measurement_t - expected_zt)                                                  # difference between observed and expected measurement
+    z_mean_diff_T = (measurement_t - expected_zt).T                                              # transpose of above
     exponential = np.exp(-0.5 * (z_mean_diff_T @ cov_inverse @ z_mean_diff))    # exponential part of the likelihood function
 
     likelihood = e1 * cov_sqrt_det * exponential                                # the likelihood L(z_t)
@@ -35,6 +50,14 @@ def state_likelihood(z_t, mean, cov):
 
 
 def sample_motion_model(particle_n):
+    """
+    Propagates particle_n through the motion model and adds a gaussian noise.
+    
+    Parameters:
+    particle_n: Hypothetical state vector; i.e the nth particle at time t x^{[n]}_t 
+    
+    Returns xn_t1: state vector at time t+1 x^{[n]}_{t+1}
+    """
     # contants and matrices for motion model
     qw = 5                                          # uncertainty in w := dtheta
     qb = 0.01                                       # uncertainty in bias 
@@ -44,12 +67,10 @@ def sample_motion_model(particle_n):
                    [0, 0, T * qb]])
     Ad = np.array([[1, 0, 0], [T, 1, 0], [0, 0, 1]])        # state transition model
     
-    print(particle_n)
-    xn_t = particle_n                                               # nth particle state at time t
     motion_noise = np.random.multivariate_normal(zero_mean, Qd)     # adding motion noise ~ N(0, Qd)
-    xn_t1 = np.dot(Ad, xn_t.T) + motion_noise                       # hypothetical nth particle state at time t + 1
+    xn_t1 = np.dot(Ad, particle_n.T) + motion_noise                 # hypothetical nth particle state at time t + 1
 
-    return 0
+    return xn_t1
 
 
 def particle_filter(particle_set_t, measurement_t):
@@ -69,32 +90,48 @@ def particle_filter(particle_set_t, measurement_t):
     n_samples, dim = particle_set_t.shape           # the number of particles and dimension of each particle
 
 
-    # constants for process and measurement model
+    # predicted_belief = dict()
+    # estimated_belief = dict()
+    # bar_Xt = []
+    # bar_Xt1 = np.empty((n_samples, 2))
+    # bar_Xt2 = np.empty((n_samples, dim))
+    # Attempts to save the particle and the weights in one place, or one array (inside for loop)
+        # temp_arr = np.array([xn_t1, weight_xn_t1])
+        # bar_Xt.append([xn_t1, weight_xn_t1])
+        # bar_Xt1[n] = 0
+        # bar_Xt2 = np.append(xn_t1, weight_xn_t1)
+        # predicted_belief[xn_t1] = weight_xn_t1
+    # bar_Xt = np.array(bar_Xt)                         # previous version used bar_Xt, array of array of particles and weights
+    # weights = bar_Xt[:, 1]                            # to be uncommented if we are using bar_Xt
 
-    rw = 10 ** (-6)                                 # uncetrainty in gyro measurements
-    r_theta = 5 * (10 ** (-5))                      # uncertainty in accel. measurements
-    Rd = np.array([[r_theta, 0],                    # measurement covariance
-                   [0, rw]])
-    C = np.array([[0, 1, 0], [1, 0, 1]])                    # measurement matrix
-
-    predicted_belief = {}
-    estimated_belief = {}
+    weights = []
+    pred_state = []
+    est_state_set = []
 
     
-    
+    # this for-loop effectively calculates \bar{X_t}, i.e. the predicted belief.
     for n in range(n_samples):
         # predicted motion step:
         xn_t1 = sample_motion_model(particle_set_t[n])
 
         # measurement correction step:
-        z_t = np.array([z_theta[n], z_dtheta[n]])
-        exp_zt = np.dot(C, xn_t1)                                       # expected z_t for each hyp. particle x_t
-        # obs_noise = np.random.multivariate_normal(exp_zt, Rd)           # adding measurement noise ~ N(Cxt, Rd)
-        weight_xn_t1 = state_likelihood(z_t, exp_zt, Rd)
+        weight_xn_t1 = state_likelihood(measurement_t, xn_t1)
 
-        print("Obs: {0} \t\tExp: {1} \t\tProb.: {2}".format(z_t, exp_zt, weight_xn_t1))
+        pred_state.append(xn_t1)
+        weights.append(weight_xn_t1)
+
+        # print("Obs: {0} \t\tExp: {1} \t\tProb.: {2}".format(z_t, mean, weight_xn_t1))
     
-    return 0
+    weights = np.array(weights)
+    pred_state = np.array(pred_state)
+
+    # the resampling step:
+    indices = monte_carlo.residual_resample(weights)
+
+    for index in indices:
+        est_state_set.append(pred_state[index])
+
+    return est_state_set
 
 
 """
@@ -103,71 +140,13 @@ in each time step we will call the particle filter algorithm
 and pass it just the particle set and enw measurement step in that time step.
 """
 
-def main():
-    """
-    Generates uniform samples to span the space of the states, 
-    and propagates through one iteration of the particle filter
-    algorithm without the resampling step.
-    """
-
-    T = 0.01                            # sampling time. new measuements at every T seconds
-    a, b = -0.5, 0.5                    # interval to generate uniform particles
-    n_samples = 100                     # number of samples
-    dim = 3                             # vector dimension for 3 states to be estimated
-    np.random.seed(0)
-    
-    qw = 5                              # uncertainty in w
-    qb = 0.01                           # uncertainty in b
-    q_01 = (T ** 2) * qw / 2
-    Qd = np.array([[T * qw, q_01, 0],                       # motion covariance
-                   [q_01, (T ** 3) * qw / 3, 0], 
-                   [0, 0, T * qb]])
-
-
-    # Actual measurements taken from the csv
-    z_theta, z_dtheta = sm.retreive_measurement()              # unit rad, rad/sec
-    rw = 10 ** (-6)
-    r_theta = 5 * (10 ** (-5))
-    Rd = np.array([[r_theta, 0],                            # measurement covariance
-                  [0, rw]])
-
-
-    Ad = np.array([[1, 0, 0], [T, 1, 0], [0, 0, 1]])        # state transition model
-    C = np.array([[0, 1, 0], [1, 0, 1]])                    # measurement matrix
-
-    zero_mean = np.array([0.0, 0.0, 0.0])                   # to construct any N(0, Cov) 
-
-
-    state_t = sm.uniform_samples(a, b, n_samples, dim)         # initialising particles to span the limits of state
-
-
-    particle_set_t = []                                                 # to hold the predicted particle set
-
-    for n in range(n_samples):
-        # predicted motion step:
-        xn_t = state_t[n]                                               # nth particle state at time t
-        motion_noise = np.random.multivariate_normal(zero_mean, Qd)     # adding motion noise ~ N(0, Qd)
-        xn_t1 = np.dot(Ad, xn_t.T) + motion_noise                       # hypothetical nth particle state at time t + 1
-
-
-        # measurement correction step:
-        z_t = np.array([z_theta[n], z_dtheta[n]])
-        exp_zt = np.dot(C, xn_t1)                                       # expected z_t for each hyp. particle x_t
-        # obs_noise = np.random.multivariate_normal(exp_zt, Rd)           # adding measurement noise ~ N(Cxt, Rd)
-        weight_xn_t1 = state_likelihood(z_t, exp_zt, Rd)
-
-        print("Obs: {0} \t\tExp: {1} \t\tProb.: {2}".format(z_t, exp_zt, weight_xn_t1))
-
-        particle_set_t.append([xn_t1, weight_xn_t1])
-    
-    # print(np.array(particle_set_t))
 
 # TODO: 1. Move weighting process before prediction to go ahead with first un-intialized particles
 # TODO: 2. Make weight and state matrices and then edit them only rather than appending to a new one.
 
 
 if __name__ == "__main__":
-    a, b = -0.5, 0.5                    # interval to generate uniform particles
+    a, b = -1.6, 0.8                    # interval to generate uniform particles
     n_samples = 10                     # number of samples
     dim = 3                             # vector dimension for 3 states to be estimated
     particle_set = sm.uniform_samples(a, b, n_samples, dim)         # initialising particles to span the limits of state
